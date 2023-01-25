f83bd1e6e33166ff9c47ccbd3b85b82f2912a590
hwan910 <psh5575@gmail.com>
2023-01-25 10:00:41 +0900

    fix 충돌해결

---
+++ src/common/color.js
@@ -0,0 +1,7 @@
+export const COLOR = {
+  YELLOW: '#fad61d',
+  MUSTAD: '#e19720',
+  RED: '#f62d14',
+  BURGUNDY: '#811E09',
+  BLACK: '#000000',
+};
--- src/shared/Layout/Header/Header.tsx
+++ src/shared/Layout/Header/Header.tsx
@@ -5,13 +5,44 @@ import {
   Logo,
   HeaderBtnBox,
   HeaderBtn,
+  SearchBox,
+  SearchInput,
+  SearchBtn,
+  LogoText,
+  LogoBox,
+  SearchSelect,
 } from './style';
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import LoginModal from '../LoginModal/LoginModal';
+import { IoSearchCircle } from 'react-icons/io5';
+import { getChargerinfo, zscode, zcode } from '../../../common/api';
 
 const Header = () => {
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
+  const [z1List, setZ1List] = useState<string[]>([]);
+  const [z1, setZ1] = useState('');
+  const [z2List, setZ2List] = useState<string[]>([]);
+  const [z2, setZ2] = useState('');
+
+  useEffect(() => {
+    const arr = [];
+    for (const [key, value] of Object.entries(zcode)) {
+      arr.push(`${key}:${value}`);
+    }
+    setZ1List(arr);
+  }, []);
+
+  const z2ListHandler = (e: any) => {
+    setZ1(e.target.value);
+    const arr = [];
+    for (const [key, value] of Object.entries(zscode)) {
+      if (key.slice(0, 2) === e.target.value) {
+        arr.push(`${key}:${value}`);
+      }
+    }
+    setZ2List(arr);
+  };
 
   const openModal = () => {
     setIsOpen(true);
@@ -20,10 +51,47 @@ const Header = () => {
   return (
     <StyledHeader>
       <HeaderContainer>
-        <Logo
-          onClick={() => navigate('/')}
-          src={require('../../../assets/Logo.png')}
-        />
+        <LogoBox onClick={() => navigate('/')}>
+          <LogoText>피카츙</LogoText>
+          <Logo src={require('../../../assets/Logo.png')} />
+        </LogoBox>
+
+        <SearchBox>
+          <SearchSelect>
+            <select
+              style={{
+                border: 'none',
+                borderRadius: 5,
+              }}
+              onChange={z2ListHandler}
+            >
+              <option value="none">=1차분류=</option>
+              {z1List.map((item, i) => (
+                <option value={item.split(':')[0]} key={i}>
+                  {item.split(':')[1]}
+                </option>
+              ))}
+            </select>
+            <select
+              style={{ border: 'none', borderRadius: 5 }}
+              onChange={(e: any) => setZ2(e.target.value)}
+            >
+              <option value="none">=2차분류=</option>
+              {z2List.map((item, i) => (
+                <option value={item.split(':')[0]} key={i}>
+                  {item.split(':')[1]}
+                </option>
+              ))}
+            </select>
+          </SearchSelect>
+          <SearchBtn>
+            <IoSearchCircle
+              color="red"
+              size={'2.6em'}
+              onClick={() => getChargerinfo(z1, z2)}
+            />
+          </SearchBtn>
+        </SearchBox>
         <HeaderBtnBox>
           <HeaderBtn onClick={() => navigate('/my')}>마이페이지</HeaderBtn>
           <HeaderBtn onClick={openModal}>LOGIN</HeaderBtn>
