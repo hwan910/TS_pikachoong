40dd9730494d24965563afe6ee47ba7edfaf77d8
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-01-26 09:08:53 +0900

    feat: 인근 충전소 찾기 2/3 정도 함

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,6 +2,9 @@
 import { useRef, useEffect, useState } from 'react';
 import { Data, GeoResult, Item, Location } from '../types/MapInterface';
 import { useNavigate } from 'react-router-dom';
+import { COLOR } from '../common/color';
+import Main from './Main';
+import styled from 'styled-components';
 
 const { kakao } = window;
 
@@ -21,6 +24,8 @@ const Map = ({ data, myLocation, setLocation, setMyLocation }: Props) => {
   const navigate = useNavigate();
   const mapRef = useRef(null);
   const [map, setMap] = useState<any>('');
+  let markers: any[] = [];
+  let arrFilter: any[] = [];
 
   useEffect(() => {
     const arrUnique = data?.items.item.filter(
@@ -32,9 +37,6 @@ const Map = ({ data, myLocation, setLocation, setMyLocation }: Props) => {
     );
     const location = new kakao.maps.LatLng(myLocation.lat, myLocation.lng);
 
-    const nearItems = arrUnique?.filter((item: Item) => item.lat);
-    console.log(myLocation);
-
     const options = {
       center: location,
       level: 2,
@@ -44,7 +46,7 @@ const Map = ({ data, myLocation, setLocation, setMyLocation }: Props) => {
     // setMap(map);
 
     const zoomControl = new kakao.maps.ZoomControl();
-    map.setDraggable(false);
+    // map.setDraggable(false);
     map.addControl(zoomControl, kakao.maps.ControlPosition.LEFT);
 
     const thunderimageSrc = require('../assets/thunder.png');
@@ -55,6 +57,7 @@ const Map = ({ data, myLocation, setLocation, setMyLocation }: Props) => {
       position: location,
       clickable: true,
     });
+
     const thunderImage = new kakao.maps.MarkerImage(thunderimageSrc, imageSize);
     const thunderoffImage = new kakao.maps.MarkerImage(
       thunderoffimageSrc,
@@ -74,6 +77,8 @@ const Map = ({ data, myLocation, setLocation, setMyLocation }: Props) => {
           position,
           image: x.stat === '2' ? thunderImage : thunderoffImage,
         });
+        markers.push(marker);
+
         kakao.maps.event.addListener(marker, 'mouseover', () => {
           overlay.setMap(map);
         });
@@ -90,19 +95,62 @@ const Map = ({ data, myLocation, setLocation, setMyLocation }: Props) => {
 
     var circle = new kakao.maps.Circle({
       map: map,
-      // center: new kakao.maps.LatLng(myLocation.Ma, myLocation.La),
+      center: new kakao.maps.LatLng(myLocation.lat, myLocation.lng),
       radius: 1000,
       strokeWeight: 2,
-      strokeColor: '#FF00FF',
+      strokeColor: `${COLOR.RED}`,
       strokeOpacity: 0.8,
       strokeStyle: 'dashed',
       fillColor: '#00EEEE',
-      fillOpacity: 0.5,
+      fillOpacity: 0.2,
     });
 
-    circle.setMap(map);
-  }, []);
+    var center = circle.getPosition();
+    var radius = circle.getRadius();
+    var line = new kakao.maps.Polyline();
 
+    let markerLocation: any[] = [];
+    markers?.forEach(function (marker) {
+      // 마커의 위치와 원의 중심을 경로로 하는 폴리라인 설정
+      console.log('marker!@#@!#@', marker);
+      var path = [marker.getPosition(), center];
+      line.setPath(path);
+
+      // 마커와 원의 중심 사이의 거리
+      var dist = line.getLength();
+
+      // 이 거리가 원의 반지름보다 작거나 같다면
+      if (dist <= radius) {
+        marker.setMap(map);
+        markerLocation.push(marker.n);
+        console.log(marker);
+      }
+    });
+
+    setMap(map);
+    circle.setMap(map);
+
+    for (const markerLocate of markerLocation) {
+      var coords = new kakao.maps.Coords(markerLocate.La, markerLocate.Ma);
+      // console.log('Marker', markerLocation);
+      // console.log('coords@@', coords);
+      const coord = coords.toLatLng();
+      // console.log('coord@@', coord);
+      let La = coord.La.toFixed(10);
+      let Ma = coord.Ma.toFixed(10);
+      console.log('LA@@', La, 'MA@@', Ma);
+
+      const filters = arrUnique?.find(
+        (item: Item) =>
+          Number(item.lat).toFixed(10) === Ma &&
+          Number(item.lng).toFixed(10) === La,
+      );
+      if (filters !== undefined) {
+        arrFilter.push(filters);
+      }
+    }
+  }, []);
+  console.log('@@', arrFilter);
   const [text, setText] = useState('');
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -116,6 +164,39 @@ const Map = ({ data, myLocation, setLocation, setMyLocation }: Props) => {
     geocoder.addressSearch(text, (result: GeoResult[], status: string) => {
       if (status === kakao.maps.services.Status.OK) {
         console.log(result[0]);
+
+        var circle = new kakao.maps.Circle({
+          map: map,
+          center: new kakao.maps.LatLng(result[0].y, result[0].x),
+          radius: 1000,
+          strokeWeight: 2,
+          strokeColor: `${COLOR.RED}`,
+          strokeOpacity: 0.8,
+          strokeStyle: 'dashed',
+          fillColor: '#00EEEE',
+          fillOpacity: 0.2,
+        });
+
+        var center = circle.getPosition();
+        var radius = circle.getRadius();
+        var line = new kakao.maps.Polyline();
+
+        markers?.forEach(function (marker) {
+          // 마커의 위치와 원의 중심을 경로로 하는 폴리라인 설정
+
+          var path = [marker.getPosition(), center];
+          line.setPath(path);
+
+          // 마커와 원의 중심 사이의 거리
+          var dist = line.getLength();
+
+          // 이 거리가 원의 반지름보다 작거나 같다면
+          if (dist <= radius) {
+            marker.setMap(map);
+            console.log(marker);
+          }
+        });
+
         let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
         new kakao.maps.Marker({
@@ -126,19 +207,47 @@ const Map = ({ data, myLocation, setLocation, setMyLocation }: Props) => {
         map.setCenter(coords);
         setLocation({ lat: result[0].y, lng: result[0].x });
         setMyLocation({ lat: result[0].y, lng: result[0].x });
+        circle.setMap(map);
       }
     });
   };
 
+  // const EARTH_RADIUS = 6371;
+  // let myLatitude = Number(myLocation.lat);
+  // let myLongitude = Number(myLocation.lng);
+
+  // //m당 y 좌표 이동 값
+  // let mForLatitude = 1 / (EARTH_RADIUS * 1 * (Math.PI / 180)) / 1000;
+  // //m당 x 좌표 이동 값
+  // let mForLongitude =
+  //   1 /
+  //   (EARTH_RADIUS *
+  //     1 *
+  //     (Math.PI / 180) *
+  //     Math.cos((myLongitude * Math.PI) / 180)) /
+  //   1000;
+
   return (
-    <>
-      <div ref={mapRef} style={{ width: 1200, height: 800 }} />
+    <Container>
       <form onSubmit={onSubmit}>
         <input type="text" onChange={(e) => onChange(e)} value={text} />
         <button>확인</button>
       </form>
-    </>
+      <div
+        ref={mapRef}
+        style={{ width: 1200, height: 800, marginBottom: '3%' }}
+      />
+      <Main filterData={arrFilter} />
+    </Container>
   );
 };
 
 export default Map;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
